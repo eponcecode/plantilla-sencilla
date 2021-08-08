8de79eb936db80bc71ad2bfd7da72d5d1c758d43
esedecita <eponcedeleonrosas@gmail.com>
2021-08-08 11:40:28 -0500

    Link added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,11 +2,12 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Link from 'react-router-dom'
 function NavbarHeader() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container>
-    <Navbar.Brand href="inicio">Tu sitio</Navbar.Brand>
+    <Navbar.Brand><Link to="/">Tu sitio</Link></Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
