f42e62d9f38e18bee5d25566bf64decc11bbd294
esedecita <eponcedeleonrosas@gmail.com>
2021-08-08 14:19:47 -0500

    Test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -7,15 +7,15 @@ function NavbarHeader() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container>
-    <Link to="/"><Navbar.Brand>Tu sitio</Navbar.Brand></Link>
+    <Link to="/plantilla-sencilla"><Navbar.Brand>Tu sitio</Navbar.Brand></Link>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="quienes-somos">¿Quiénes somos?</Nav.Link>
-        <Nav.Link href="historia">Historia</Nav.Link>
-        <Nav.Link href="servicios">Servicios</Nav.Link>
-        <Nav.Link href="galeria">Galeria</Nav.Link>
-        <Nav.Link href="contacto">Contacto</Nav.Link>
+      <Link to="plantilla-sencilla/quienes-somos"><Nav.Link>¿Quiénes somos?</Nav.Link></Link>
+        <Link to="plantilla-sencilla/historia"><Nav.Link>Historia</Nav.Link></Link>
+        <Link to="plantilla-sencilla/servicios"><Nav.Link>Servicios</Nav.Link></Link>
+        <Link to="plantilla-sencilla/galeria"><Nav.Link>Galeria</Nav.Link></Link>
+        <Link to="plantilla-sencilla/contacto"><Nav.Link>Contacto</Nav.Link></Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
