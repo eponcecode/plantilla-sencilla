03a857ea9445d10dcf3fb1047bc5db5beeadab39
esedecita <eponcedeleonrosas@gmail.com>
2021-08-10 10:19:34 -0500

    test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,26 +1,38 @@
+import {Component} from 'react' 
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {Link} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 
-function NavbarHeader() {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+class NavbarHeader extends Component {
+  render() { 
+    return ( <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container >
      <Navbar.Brand componentclass={Link} to="/plantilla-sencilla/inicio" href="/plantilla-sencilla/inicio">Tu sitio</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
+      <BrowserRouter>
         <Nav.Link  componentclass={Link} to="/plantilla-sencilla/quienes-somos" href="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</Nav.Link>
+        </BrowserRouter>
+        <BrowserRouter>
          <Nav.Link componentclass={Link} to="/plantilla-sencilla/historia" href="/plantilla-sencilla/historia">Historia </Nav.Link>
+         </BrowserRouter>
+        <BrowserRouter>
          <Nav.Link componentclass={Link} to="/plantilla-sencilla/servicios" href="/plantilla-sencilla/servicios">Servicios </Nav.Link>
+         </BrowserRouter>
+        <BrowserRouter>
          <Nav.Link componentclass={Link} to="/plantilla-sencilla/galeria" href="/plantilla-sencilla/galeria">Galeria </Nav.Link>
+         </BrowserRouter>
+        <BrowserRouter>
          <Nav.Link componentclass={Link} to="/plantilla-sencilla/contacto" href="/plantilla-sencilla/contacto">Contacto </Nav.Link>
+         </BrowserRouter>
       </Nav>
     </Navbar.Collapse>
   </Container>
-</Navbar>);
+</Navbar> );
+  }
 }
 
 export default NavbarHeader;
