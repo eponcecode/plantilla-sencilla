5b1654e4f950360a69cc88883c429161abc2d723
esedecita <eponcedeleonrosas@gmail.com>
2021-08-10 10:26:08 -0500

    test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,31 +3,21 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 class NavbarHeader extends Component {
   render() { 
     return ( <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container >
-     <Navbar.Brand componentclass={Link} to="/plantilla-sencilla/inicio" href="/plantilla-sencilla/inicio">Tu sitio</Navbar.Brand>
+     <NavLink to="/plantilla-sencilla/inicio" href="/plantilla-sencilla/inicio">Tu sitio</NavLink>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-      <BrowserRouter>
-        <Nav.Link  componentclass={Link} to="/plantilla-sencilla/quienes-somos" href="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</Nav.Link>
-        </BrowserRouter>
-        <BrowserRouter>
-         <Nav.Link componentclass={Link} to="/plantilla-sencilla/historia" href="/plantilla-sencilla/historia">Historia </Nav.Link>
-         </BrowserRouter>
-        <BrowserRouter>
-         <Nav.Link componentclass={Link} to="/plantilla-sencilla/servicios" href="/plantilla-sencilla/servicios">Servicios </Nav.Link>
-         </BrowserRouter>
-        <BrowserRouter>
-         <Nav.Link componentclass={Link} to="/plantilla-sencilla/galeria" href="/plantilla-sencilla/galeria">Galeria </Nav.Link>
-         </BrowserRouter>
-        <BrowserRouter>
-         <Nav.Link componentclass={Link} to="/plantilla-sencilla/contacto" href="/plantilla-sencilla/contacto">Contacto </Nav.Link>
-         </BrowserRouter>
+        <NavLink  to="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</NavLink>
+         <NavLink to="/plantilla-sencilla/historia">Historia </NavLink>
+         <NavLink to="/plantilla-sencilla/servicios">Servicios </NavLink>
+         <NavLink to="/plantilla-sencilla/galeria">Galeria </NavLink>
+         <NavLink to="/plantilla-sencilla/contacto">Contacto </NavLink>
       </Nav>
     </Navbar.Collapse>
   </Container>
