b6004a9f3caaac94c244887800633d336a4532d6
esedecita <eponcedeleonrosas@gmail.com>
2021-08-10 10:04:34 -0500

    test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,21 +2,21 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter,NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 function NavbarHeader() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container >
-     <NavLink activeClassName='navbar-brand' to="/plantilla-sencilla/inicio">Tu sitio</NavLink>
+     <Navbar.Brand componentclass={Link} to="/plantilla-sencilla/inicio" href="/plantilla-sencilla/inicio">Tu sitio</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <NavLink activeClassName='nav-link' to="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</NavLink>
-         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/historia">Historia </NavLink>
-         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/servicios">Servicios </NavLink>
-         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/galeria">Galeria </NavLink>
-         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/contacto">Contacto </NavLink>
+        <Nav.Link  componentclass={Link} to="/plantilla-sencilla/quienes-somos" href="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</Nav.Link>
+         <Nav.Link componentclass={Link} to="/plantilla-sencilla/historia" href="/plantilla-sencilla/historia">Historia </Nav.Link>
+         <Nav.Link componentclass={Link} to="/plantilla-sencilla/servicios" href="/plantilla-sencilla/servicios">Servicios </Nav.Link>
+         <Nav.Link componentclass={Link} to="/plantilla-sencilla/galeria" href="/plantilla-sencilla/galeria">Galeria </Nav.Link>
+         <Nav.Link componentclass={Link} to="/plantilla-sencilla/contacto" href="/plantilla-sencilla/contacto">Contacto </Nav.Link>
       </Nav>
     </Navbar.Collapse>
   </Container>
