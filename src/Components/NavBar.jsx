3061edfbdeaa2e06e8e20a964bbe268dc05a91dc
esedecita <eponcedeleonrosas@gmail.com>
2021-08-09 21:06:28 -0500

    test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,17 +8,15 @@ function NavbarHeader() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container >
-    <BrowserRouter>
-     <NavLink activeClassName='' to="/plantilla-sencilla/inicio">Tu sitio</NavLink>
-     </BrowserRouter>
+     <NavLink activeClassName='navbar-brand' to="/plantilla-sencilla/inicio">Tu sitio</NavLink>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <NavLink activeclassName='' to="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</NavLink>
-         <NavLink activeclassName='' to="/plantilla-sencilla/historia">Historia </NavLink>
-         <NavLink activeclassName='' to="/plantilla-sencilla/servicios">Servicios </NavLink>
-         <NavLink activeclassName='' to="/plantilla-sencilla/galeria">Galeria </NavLink>
-         <NavLink activeclassName='' to="/plantilla-sencilla/contacto">Contacto </NavLink>
+        <NavLink activeClassName='nav-link' to="/plantilla-sencilla/quienes-somos">¿Quiénes somos?</NavLink>
+         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/historia">Historia </NavLink>
+         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/servicios">Servicios </NavLink>
+         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/galeria">Galeria </NavLink>
+         <NavLink activeClassName='nav-link' to="/plantilla-sencilla/contacto">Contacto </NavLink>
       </Nav>
     </Navbar.Collapse>
   </Container>
