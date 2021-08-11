7faaf5817776732a7b9a9c42cb8f676a1b089ee6
esedecita <eponcedeleonrosas@gmail.com>
2021-08-11 17:02:18 -0500

    test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {NavLink} from 'react-router-dom';
+import {NavLink,BrowserRouter as Route} from 'react-router-dom';
 
 class NavbarHeader extends Component {
   render() { 
     return ( <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container >
+      <Route>
      <NavLink className='navbar-brand' to="/plantilla-sencilla/inicio">Tu sitio</NavLink>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
@@ -20,6 +21,7 @@ class NavbarHeader extends Component {
          <NavLink className='nav-item nav-link' to="/plantilla-sencilla/contacto">Contacto </NavLink>
       </Nav>
     </Navbar.Collapse>
+    </Route>
   </Container>
 </Navbar> );
   }
