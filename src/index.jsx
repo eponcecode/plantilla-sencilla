83fdf50d30fa534f7585e4aa724b31e7ec837c59
esedecita <eponcedeleonrosas@gmail.com>
2021-08-11 17:57:26 -0500

    test

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
     <App />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('tusitio')
 );
