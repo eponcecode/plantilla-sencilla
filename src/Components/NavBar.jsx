84cbd698b65414ade460edf01c8ac4112f99e027
esedecita <eponcedeleonrosas@gmail.com>
2021-08-13 18:43:03 -0500

    test

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,15 +9,15 @@ class NavbarHeader extends Component {
   render() { 
     return ( <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
     <Container >
-     <NavLink className='navbar-brand' to="/inicio">Tu sitio</NavLink>
+     <NavLink className='navbar-brand' to="/#/inicio">Tu sitio</NavLink>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <NavLink className='nav-item nav-link' to="/quienes-somos">¿Quiénes somos?</NavLink>
-         <NavLink className='nav-item nav-link' to="/historia">Historia </NavLink>
-         <NavLink className='nav-item nav-link' to="/servicios">Servicios </NavLink>
-         <NavLink className='nav-item nav-link' to="/galeria">Galeria </NavLink>
-         <NavLink className='nav-item nav-link' to="/contacto">Contacto </NavLink>
+        <NavLink className='nav-item nav-link' to="/#/quienes-somos">¿Quiénes somos?</NavLink>
+         <NavLink className='nav-item nav-link' to="/#/historia">Historia </NavLink>
+         <NavLink className='nav-item nav-link' to="/#/servicios">Servicios </NavLink>
+         <NavLink className='nav-item nav-link' to="/#/galeria">Galeria </NavLink>
+         <NavLink className='nav-item nav-link' to="/#/contacto">Contacto </NavLink>
       </Nav>
     </Navbar.Collapse>
   </Container>
